de240d1b32896334ca976acb551f7cf766051aa2
anaypant <35899820+anaypant@users.noreply.github.com>
2025-05-29 13:07:11 -0700

    Create ev_calculator.py

---
+++ ev_calculator.py
@@ -0,0 +1,57 @@
+# ev_calculator.py
+import requests
+
+def calc_ev(messages: list) -> int:
+    tai_key = '2e1a1e910693ae18c09ad0585a7645e0f4595e90ec35bb366b6f5520221b6ca7'
+    url = "https://api.together.xyz/v1/chat/completions"
+
+    headers = {
+            "Authorization": f"Bearer {tai_key}",
+            "Content-Type": "application/json"
+    }
+    payload_messages = [{
+        "role": "system",
+        "content": '''The assistant should only respond with a number. This number will be in the range from 0-100. This number will indicate how interested a prospective buyer of a property is interested in it. The assistant will do this buy looking at a series of emails. These emails will be from the prospective buyer and an realtor helping the buyer. Each email will be sent as a separate message by the user, and the first word will indicate if the email was sent by a buyer or an realtor. 
+                    If the first word is BUYER: the email that follows is sent by a buyer. 
+                    If the first word is REALTOR: the email that follows is sent by a realtor. 
+                    The assistant will analyze the given information, and reply with the number that indicates the interest of the prospective buyer of the property.
+                    Regardless of the input, reply with a number 0-100.'''
+    }]
+    payload_messages.extend(messages)
+    payload = {
+        "model": "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
+        "messages": payload_messages,
+        "max_tokens": 5,
+        "temperature": 0.7,
+        "top_p": 0.7,
+        "top_k": 50,
+        "repetition_penalty": 1,
+        "stop": ["<|im_end|>", "<|endoftext|>"],
+        "stream": False
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    response_data = response.json()
+        
+    if response.status_code != 200 or "choices" not in response_data:
+        print ("Failed to fetch response from Together AI API", response_data)
+        return -3
+
+    print("EV Response: " + response_data["choices"][0]["message"]["content"])
+    try:
+        ev = int(response_data["choices"][0]["message"]["content"])
+        return ev
+    except ValueError:
+        print('The AI did not return a valid number')
+        return -2
+
+
+def parse_messages(realtor_email: str, emails: list) -> list:
+    messages = []
+    for email in emails:
+        if email['sender'].split('@')[1] == 'lgw.automatedconsultancy.com':
+            continue
+        messages.append({'role': 'user', 'content':  
+                                        ('REALTOR: ' if email['sender'] == realtor_email else 'BUYER: ')
+                                        + email['body']})
+    return messages
