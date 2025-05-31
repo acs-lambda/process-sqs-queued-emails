70280ed92d318713ef0229d80319a01792ab7435
Anay Pant <24apant@athenian.org>
2025-05-31 02:55:42 -0700

    Add new Lambda ARNs to config and refactor database interactions in db.py and lambda_function.py to utilize db-select for data operations. Implement error handling and logging improvements for better traceability.

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ import os
 BUCKET_NAME = os.environ['BUCKET_NAME']
 QUEUE_URL = os.environ['QUEUE_URL']
 PROCESSING_LAMBDA_ARN = os.environ['PROCESSING_LAMBDA_ARN']
+GENERATE_EV_LAMBDA_ARN = os.environ['GENERATE_EV_LAMBDA_ARN']
+LCP_LLM_RESPONSE_LAMBDA_ARN = os.environ['LCP_LLM_RESPONSE_LAMBDA_ARN']
+DB_SELECT_LAMBDA = os.environ['DB_SELECT_LAMBDA']  # Database Lambda function name
 AWS_REGION = os.environ.get('AWS_REGION', 'us-east-2')
